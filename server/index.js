bfccac6f90ef3fa4d94549c23367943c5580a830
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2023-01-17 11:02:47 +0000

    not using botkit

--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Ladies and Gentlemen, this is ur own chat bot!');
+});
+
+app.post('/chat', (req, res) => {
+    
+});
+
+
+app.listen(port, () => {
+    console.log(`Bot is running on port ${port}`);
+}
+);
